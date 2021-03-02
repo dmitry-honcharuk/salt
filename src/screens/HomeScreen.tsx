72708af99f51b6d64e161c79c25545da5726f401
Dmitry Honcharuk <dmitry.honcharuk@gmail.com>
2021-03-02 23:57:35 +0200

    pwa state test

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,10 +1,19 @@
 import { List } from 'core/entities/List';
-import { FunctionComponent } from 'react';
+import { FunctionComponent, useState } from 'react';
 
 export type Props = {
   lists: List[];
 };
 
 export const HomeScreen: FunctionComponent<Props> = ({ lists }) => {
-  return <h1>Well hello there</h1>;
+  const [count, setCount] = useState(0);
+
+  const increment = () => setCount((i) => i + 1);
+
+  return (
+    <>
+      <h1>Well hello there ({count})</h1>
+      <button onClick={increment}>Click</button>
+    </>
+  );
 };
