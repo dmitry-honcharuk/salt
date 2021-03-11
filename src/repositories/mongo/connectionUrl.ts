4f7ef66a295855a1d082eb1f5fca866fc4afdbb3
Dmitry Honcharuk <dmitry.honcharuk@gmail.com>
2021-03-11 21:42:26 +0200

    fix connection url

--- src/repositories/mongo/connectionUrl.ts
+++ src/repositories/mongo/connectionUrl.ts
@@ -1,1 +1,1 @@
-export const CONNECTION_URL = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PPASSWORD}@salt.3okfj.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+export const CONNECTION_URL = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@salt.3okfj.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
