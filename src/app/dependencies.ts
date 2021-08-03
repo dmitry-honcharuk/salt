cf83dfaf45bfe9f193a4ea7e6ffdc7d8249e8b68
Dmitry Honcharuk <dmitry.honcharuk@gmail.com>
2021-08-04 01:58:26 +0300

    bumped @ficdev/react-auth version & removed unused code

--- src/app/dependencies.ts
+++ src/app/dependencies.ts
@@ -1,12 +1,8 @@
 import { authorizedMiddlewareFactory } from '@ficdev/auth-express';
 import { buildMongoListRepository } from 'app/implementations/repositories/mongo/ListRepository';
 import { NEXT_PUBLIC_AUTH_BASE_URL, NEXT_PUBLIC_AUTH_CLIENT_ID } from './config/env';
-import { buildAppAuthServiceFactory } from './implementations/services/appAuthService';
 
 export const listRepository = buildMongoListRepository();
-export const appAuthServiceFactory = buildAppAuthServiceFactory({
-  clientId: NEXT_PUBLIC_AUTH_CLIENT_ID,
-});
 export const authorized = authorizedMiddlewareFactory({
   clientId: NEXT_PUBLIC_AUTH_CLIENT_ID,
   authUrlBase: NEXT_PUBLIC_AUTH_BASE_URL,
